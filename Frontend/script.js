7adbe44cb4faad06744f652ec474c80b67bc9ab6
moo712 <102692275+moo712@users.noreply.github.com>
2022-05-25 11:47:03 +0200

    Delete Frontend directory

--- Frontend/script.js
+++
@@ -1,6 +0,0 @@
-let loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-window.onload = function () {
-  let loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-  console.log("Page load time is " + loadTime);
-  document.getElementById("time").innerText = "Page load time is " + loadTime + "ms";
-};
